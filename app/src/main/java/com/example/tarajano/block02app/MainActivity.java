e888e26a98b01333deb7f170bc565ec7cde2249c
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-21 22:36:06 -0400

    SUMMARY: Ads an ArrayAdaptor and assoc it to animalsListView. REMARKS: coursera.org/learn/android-app TODO:
    FILE: MainActivity
      ADDS: ArrayAdapter<CharSequence> adapterAnimals
      ADDS: Association adapterAnimals to listViewAnimals.
    
    FILE: activity_main
      MODIFIES: Font sizes using dimentions.xml
    
    FILE: dimensions.xml
      ADDS: This file contains font sizes for the layout

--- app/src/main/java/com/example/tarajano/block02app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block02app/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.tarajano.block02app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listViewAnimals = (ListView) findViewById(R.id.animalsListView);
+        ArrayAdapter<CharSequence> adapterAnimals = ArrayAdapter.createFromResource(
+                this, R.array.animalsArray, android.R.layout.simple_expandable_list_item_1);
+        listViewAnimals.setAdapter(adapterAnimals);
+
     }
 }
