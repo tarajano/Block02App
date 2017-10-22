63b562776eafcac7f3a8771a313d4d722c9571a0
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-21 23:25:15 -0400

    SUMMARY: Assoc onClick actions to the animalsListView. REMARKS: coursera.org/learn/android-app TODO:
    FILE:   app/src/main/java/com/example/tarajano/block02app/MainActivity.java
      ADDS: setOnItemClickListener() to assoc Toast popup actions to the
      list items.
    
    FILE:   app/src/main/res/values-es/strings.xml
      ADDS: bird item (w/o providing translation to Spanish)

--- app/src/main/java/com/example/tarajano/block02app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block02app/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.tarajano.block02app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import javax.xml.datatype.Duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listViewAnimals = (ListView) findViewById(R.id.animalsListView);
-        ArrayAdapter<CharSequence> adapterAnimals = ArrayAdapter.createFromResource(
+        final ArrayAdapter<CharSequence> adapterAnimals = ArrayAdapter.createFromResource(
                 this, R.array.animalsArray, android.R.layout.simple_expandable_list_item_1);
         listViewAnimals.setAdapter(adapterAnimals);
 
+        // Assign actions to list items.
+        listViewAnimals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String message = getString(R.string.listItemPopUpMessage) + adapterAnimals.getItem(position);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
